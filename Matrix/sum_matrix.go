fb8e59390cb764a3eae83e5d80f954cbce3a7972
“Tanya <cutanyasanganan@gmail.com>
2022-02-17 16:45:31 +0700

    first commit

---
+++ Matrix/sum_matrix.go
@@ -0,0 +1,30 @@
+//
+package main
+
+import "fmt"
+
+func main() {
+	matrix1 := [][]int{
+		{1, 2, 3},
+		{4, 5, 6},
+		{7, 8, 9},
+	}
+	matrix2 := [][]int{
+		{1, 2, 3},
+		{1, 2, 3},
+		{1, 2, 3},
+	}
+	n_rows := len(matrix1)
+	n_columns := len(matrix1[0])
+	sum_matrix := [][]int{
+		{0, 0, 0},
+		{0, 0, 0},
+		{0, 0, 0},
+	}
+	for r := 0; r < n_rows; r++ {
+		for c := 0; c < n_columns; c++ {
+			sum_matrix[r][c] = matrix1[r][c] + matrix2[r][c]
+		}
+	}
+	fmt.Println("Sum of Matrix:", sum_matrix)
+}
